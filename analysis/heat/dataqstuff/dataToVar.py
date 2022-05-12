3e1d5d378538805e8846e9b016b008d1d7f75ed7
josh <whitehead.josh98@gmail.com>
2022-05-12 12:22:41 -0600

    d

--- analysis/heat/dataqstuff/dataToVar.py
+++ analysis/heat/dataqstuff/dataToVar.py
@@ -61,3 +61,5 @@ break105 = gd.getData('lid0_break_contAcq_105.csv','05Apr2022')
 uL700 = gd.getData('lidB700uL_sealed_c.csv','06May2022')
 uL800 = gd.getData('lidB800uL_sealed_c.csv','06May2022')
 uL900 = gd.getData('lidB900uL_sealed_b.csv','06May2022')
+
+
