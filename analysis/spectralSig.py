3cfb56c62b53e1b96148bac297cb9e0ab323aea9
Josh Whitehead <j.whitehead@co-dx.com>
2022-09-07 13:41:36 -0600

    new file:   analysis/specSigKristin/25Aug2022/0b6520.png 	new file:   analysis/specSigKristin/25Aug2022/105b6f.png 	new file:   analysis/specSigKristin/25Aug2022/c37c2b.png 	modified:   analysis/spectralSig.py

--- analysis/spectralSig.py
+++ analysis/spectralSig.py
@@ -9,119 +9,121 @@ from melt.melt import MeltAnalysis
 from scipy.interpolate import make_interp_spline
 
 
-consumableId = 'c37c2b73-193c-4ffc-8119-1a7bca36a03a'
-
-note = ''
-
-refChan = 3
-api = auth.getApiClient()
-
-where = {
-'id': {
-    '_eq': consumableId
-}
-}
-channels = [480,515,555,590,630,680]
-channelString = []
+Id = ['0b6520df-69cb-4182-ad7b-bf59f8ec90ca','105b6f63-334b-4403-a27c-a5ee07c9d2ef']
+for consumableId in Id:
 
 
-for i in channels:
-    channelString.append(str(i))
+    note = ''
+
+    refChan = 3
+    api = auth.getApiClient()
+
+    where = {
+    'id': {
+        '_eq': consumableId
+    }
+    }
+    channels = [480,515,555,590,630,680]
+    channelString = []
 
 
-rep = 0
-response = api.execute(query=qry.consumableWhere,variables={'where':where})
-# print(response)
+    for i in channels:
+        channelString.append(str(i))
 
-rawData = response['data']['consumable'][0]['experiments'][rep]['experimentResult']['pcrData']['sensor']
-data = np.array(rawData).T.tolist()
-meltData = response['data']['consumable'][0]['experiments'][rep]['experimentResult']['meltData']['sensor']
-data_melt = np.array(meltData).T.tolist()
-# print(meltData.keys())
-if len(data_melt) > 0:
-    tData = response ['data']['consumable'][0]['experiments'][rep]['experimentResult']['meltData']['temperature']
-    mData = data_melt
-consumableName = 'lambda'
-configuration = getConfigMap(consumableName)
-meltAnalysis = MeltAnalysis(configuration)
-if len(data_melt) > 0:
-    meltAnalysis.callMelt(tData, mData)
 
-pcrAnalysis = pcr.PCRAnalysis(configuration)
-if len(data) > 0:
-    pcrAnalysis.callPCR(data)
-    anneal = response['data']['consumable'][0]['experiments'][rep]['experimentResult']['pcrData']['temperature']
+    rep = 0
+    response = api.execute(query=qry.consumableWhere,variables={'where':where})
+    # print(response)
 
-def pcrplot():
-    delta = []
-    for i in [2,3,4,5,6,7]:
-        delta.append(np.average(data[i][-2:])-np.average(data[i][:2]))
+    rawData = response['data']['consumable'][0]['experiments'][rep]['experimentResult']['pcrData']['sensor']
+    data = np.array(rawData).T.tolist()
+    meltData = response['data']['consumable'][0]['experiments'][rep]['experimentResult']['meltData']['sensor']
+    data_melt = np.array(meltData).T.tolist()
+    # print(meltData.keys())
+    if len(data_melt) > 0:
+        tData = response ['data']['consumable'][0]['experiments'][rep]['experimentResult']['meltData']['temperature']
+        mData = data_melt
+    consumableName = 'lambda'
+    configuration = getConfigMap(consumableName)
+    meltAnalysis = MeltAnalysis(configuration)
+    if len(data_melt) > 0:
+        meltAnalysis.callMelt(tData, mData)
 
-    # print(delta)
+    pcrAnalysis = pcr.PCRAnalysis(configuration)
+    if len(data) > 0:
+        pcrAnalysis.callPCR(data)
+        anneal = response['data']['consumable'][0]['experiments'][rep]['experimentResult']['pcrData']['temperature']
+
+    def pcrplot():
+        delta = []
+        for i in [2,3,4,5,6,7]:
+            delta.append(np.average(data[i][-2:])-np.average(data[i][:2]))
+
+        # print(delta)
+        
+
+        x = np.array(channels)
+        y = np.array(delta[:len(channels)])
+        frac = []
+        tot = sum(delta)
+        for i in delta:
+            frac.append(i/tot)
+
+
+        X_Y_Spline = make_interp_spline(x, y)
+
+        # Returns evenly spaced numbers
+        # over a specified interval.
+        X_ = np.linspace(x.min(), x.max(), 500)
+        Y_ = X_Y_Spline(X_)
+
+        # Plotting the Graph
+        # plt.figure()
+        plt.plot(X_, Y_,'-',x,y,'o')
+        plt.title(''.join([note,' ',consumableId[:6]]))
+        plt.xlabel("Channel (nm)")
+        plt.ylabel("Fluorescence")
+        
+        plt.xticks(channels,channelString)
+        plt.grid()
+        plt.savefig(''.join(['specSigKristin/25Aug2022/',consumableId[:6]]))
+        plt.show()
+
+
+    def melt_deriv():
+
+        x = np.array(channels)
+
+        
+        maxFluor = max(meltAnalysis.results['smoothDerivatives'][refChan])
+        maxFluor = max(meltAnalysis.results['smoothDerivatives'][refChan][100:])
+        tmIndx = list(meltAnalysis.results['smoothDerivatives'][refChan]).index(maxFluor)
+        good_tm = meltAnalysis.results['smoothedT'][tmIndx]
+        fluor = []
+        for i in [2,3,4,5,6,7]:
+            fluor.append(meltAnalysis.results['smoothDerivatives'][i][tmIndx])
+        y = np.array(fluor)
+
+
+
+        X_Y_Spline = make_interp_spline(x,y)
+
     
+        X_ = np.linspace(x.min(), x.max(), 500)
+        Y_ = X_Y_Spline(X_)
 
-    x = np.array(channels)
-    y = np.array(delta[:len(channels)])
-    frac = []
-    tot = sum(delta)
-    for i in delta:
-        frac.append(i/tot)
-
-
-    X_Y_Spline = make_interp_spline(x, y)
-
-    # Returns evenly spaced numbers
-    # over a specified interval.
-    X_ = np.linspace(x.min(), x.max(), 500)
-    Y_ = X_Y_Spline(X_)
-
-    # Plotting the Graph
-    plt.figure()
-    plt.plot(X_, Y_,'-',x,y,'o')
-    plt.title(''.join([note,' ',consumableId[:6]]))
-    plt.xlabel("Channel (nm)")
-    plt.ylabel("Fluorescence")
-    
-    plt.xticks(channels,channelString)
-    plt.grid()
-    plt.savefig(''.join(['specSigKristin/25Aug2022/',consumableId[:6]]))
-    plt.show()
-
-
-def melt_deriv():
-
-    x = np.array(channels)
-
-    
-    maxFluor = max(meltAnalysis.results['smoothDerivatives'][refChan])
-    maxFluor = max(meltAnalysis.results['smoothDerivatives'][refChan][100:])
-    tmIndx = list(meltAnalysis.results['smoothDerivatives'][refChan]).index(maxFluor)
-    good_tm = meltAnalysis.results['smoothedT'][tmIndx]
-    fluor = []
-    for i in [2,3,4,5,6,7]:
-        fluor.append(meltAnalysis.results['smoothDerivatives'][i][tmIndx])
-    y = np.array(fluor)
-
-
-
-    X_Y_Spline = make_interp_spline(x,y)
-
-   
-    X_ = np.linspace(x.min(), x.max(), 500)
-    Y_ = X_Y_Spline(X_)
-
-    # plt.plot(meltAnalysis.results['smoothedT'],meltAnalysis.results['smoothDerivatives'][4])
-    plt.figure()
-    plt.plot(X_,Y_)
-    plt.plot(np.array(channels),fluor,'o')
-    plt.title(''.join(['Spectral Signature ',consumableId[:6]]))
-    plt.ylabel(''.join(["Melt Peak @ ",str(round(good_tm,1))]))
-    plt.xlabel('Channel (nm)')
-    # plt.ylim(-.15,2.2)
-    plt.xticks((515,555,590,630,680),('515','555','590','630','680'))
-    plt.grid()
-    
-    
-    plt.savefig(''.join(['specSigAli/28Apr2022/',consumableId[:6]]))
-    plt.show()
-melt_deriv()
+        # plt.plot(meltAnalysis.results['smoothedT'],meltAnalysis.results['smoothDerivatives'][4])
+        plt.figure()
+        plt.plot(X_,Y_)
+        plt.plot(np.array(channels),fluor,'o')
+        plt.title(''.join(['Spectral Signature ',consumableId[:6]]))
+        plt.ylabel(''.join(["Melt Peak @ ",str(round(good_tm,1))]))
+        plt.xlabel('Channel (nm)')
+        # plt.ylim(-.15,2.2)
+        plt.xticks((515,555,590,630,680),('515','555','590','630','680'))
+        plt.grid()
+        
+        
+        plt.savefig(''.join(['specSigAli/28Apr2022/',consumableId[:6]]))
+        plt.show()
+    pcrplot()
