c7121679259e63e587bd87f6ca72cd889c67f230
josh <whitehead.josh98@gmail.com>
2022-11-08 11:40:37 -0700

    'q'

---
+++ analysis/qdot/statsFmaxNoise.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+data = pd.read_csv('f07Snp.csv')
+
+inst = (data['inst']).tolist()
+fmax = (data['fmax']).tolist()
+noisePcr = (data['pcr noise']).tolist()
+noiseMelt = (data['melt noise']).tolist()
+
+instSimp = [*set(inst)]
+
+fmaxGood = []
+instGood = []
+pcrGood = []
+meltGood = []
+for i in range(len(fmax)):
+    if fmax[i] >= 0:
+        fmaxGood.append(fmax[i])
+        instGood.append(fmax[i])
+        pcrGood.append(fmax[i])
+        meltGood.append(fmax[i])
+
+together = zip(instGood,fmaxGood,pcrGood,meltGood)
+
+togetherSort = sorted(together)
+
+
+
+
+
