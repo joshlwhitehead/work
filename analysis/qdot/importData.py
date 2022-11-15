bd7c3b93ca47e1fe2bea82a98dd473e7891ebf4f
josh <whitehead.josh98@gmail.com>
2022-11-15 11:47:47 -0700

    'pmv'

--- analysis/qdot/importData.py
+++ analysis/qdot/importData.py
@@ -11,9 +11,17 @@ data25 = pd.read_excel('qDotJosh.xlsx','25 LED')
 data26 = pd.read_excel('qDotJosh.xlsx','26 LED')
 data27 = pd.read_excel('qDotJosh.xlsx','27 LED')
 
+data1 = pd.read_excel('qDotJosh.xlsx','01 Laser')
+data2 = pd.read_excel('qDotJosh.xlsx','02 Laser')
+data8 = pd.read_excel('qDotJosh.xlsx','08 Laser')
+data11 = pd.read_excel('qDotJosh.xlsx','11 Laser')
+data12 = pd.read_excel('qDotJosh.xlsx','12 Laser')
+data15 = pd.read_excel('qDotJosh.xlsx','15 Laser')
 
-instData = [data5,data6,data7,data10,data13,data25,data26,data27]
-inst = [5,6,7,10,13,25,26,27]
+
+
+instData = [data1,data2,data5,data6,data7,data8,data10,data11,data12,data13,data15,data25,data26,data27]
+inst = [1,2,5,6,7,8,10,11,12,13,15,25,26,27]
 
 
 avg = []
@@ -21,7 +29,7 @@ for i in instData:
     avg.append(i['avg'][14])
 avg = np.array(avg)
 
-plt.scatter(inst,avg)
-plt.grid()
-plt.show()
+# plt.scatter(inst,avg)
+# plt.grid()
+# plt.show()
 
--- analysis/qdot/statsFmaxNoise.py
+++ analysis/qdot/statsFmaxNoise.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import importData
 
-data = pd.read_csv('f07Snp.csv')
+data = pd.read_csv('pmmv_255.csv')
 
 inst = (data['inst']).tolist()
 fmax = (data['fmax']).tolist()
 noisePcr = (data['pcr noise']).tolist()
 noiseMelt = (data['melt noise']).tolist()
+cq = (data['cq']).tolist()
+tm = (data['tm']).tolist()
 
 instSimp = [*set(inst)]
 
@@ -16,12 +18,18 @@ fmaxGood = []
 instGood = []
 pcrGood = []
 meltGood = []
-for i in range(len(fmax)):
-    if fmax[i] >= 0 and str(noisePcr[i]) != 'nan' and str(noiseMelt[i]) != 'nan' and str(fmax[i] != 'nan'):
+cqGood = []
+tmGood = []
+for i in range(len(inst)):
+    if str(noisePcr[i]) != 'nan' and str(noiseMelt[i]) != 'nan' and str(fmax[i] != 'nan') and str(cq[i]) != 'nan' and str(tm[i]) != 'nan' and str(noiseMelt[i]) != '  ' and str(cq[i]) != ' ' and str(tm[i]) != ' ':
         fmaxGood.append(fmax[i])
         instGood.append(inst[i])
         pcrGood.append(float(noisePcr[i]))
+        # print(noiseMelt[i])
         meltGood.append(float(noiseMelt[i]))
+        cqGood.append(float(cq[i]))
+
+        tmGood.append(float(tm[i]))
 
 
 
@@ -29,58 +37,39 @@ for i in range(len(fmax)):
 fmaxMean = []
 pcrMean = []
 meltMean = []
+cqMean = []
+tmMean = []
 
 for i in instSimp:
     mean1 = []
     mean2 = []
     mean3 = []
+    mean4 = []
+    mean5 = []
     for u in range(len(instGood)):
         if instGood[u] == i:
             mean1.append(fmaxGood[u])
             mean2.append(pcrGood[u])
             mean3.append(meltGood[u])
+            mean4.append(cqGood[u])
+            mean5.append(tmGood[u])
     fmaxMean.append(np.mean(mean1))
     pcrMean.append(np.mean(mean2))
     meltMean.append(np.mean(mean3))
+    cqMean.append(np.mean(mean4))
+    tmMean.append(np.mean(mean5))
 
 
-instQdot = [5,6,7,10,13,25,26,27]
+qdotInst = []
+qdotFmax = []
+for i in range(len(importData.inst)):
+    if importData.inst[i] in instSimp:
+        qdotInst.append(importData.inst[i])
+        qdotFmax.append(importData.avg[i])
 
-qdotfmax = []
-qdotpcr = []
-qdotmelt = []
-for i in range(len(instSimp)):
-    if instSimp[i] in instQdot:
-        qdotfmax.append(fmaxMean[i])
-        qdotpcr.append(pcrMean[i])
-        qdotmelt.append(meltMean[i])
-# plt.scatter(instSimp,fmaxMean)
-# plt.grid()
-# plt.show()
-
-# plt.plot(instSimp,pcrMean)
-# plt.plot(instSimp,meltMean)
-# plt.show()
-
-
-# plt.scatter(instGood,fmaxGood)
-# plt.xlabel('Instrument')
-# plt.ylabel('fmax')
-# plt.grid()
-# plt.show()
-
-
-# plt.scatter(instGood,pcrGood)
-# plt.scatter(instGood,meltGood)
-# plt.xlabel('Instrument')
-# plt.ylabel('noise')
-# plt.grid()
-# plt.show()
-
-
-plt.scatter(importData.avg,qdotmelt)
-plt.xlabel('qdot 515/480')
-plt.ylabel('snp pcr noise')
-plt.grid()
+fmaxMean.remove(fmaxMean[3])
+plt.plot(qdotFmax,fmaxMean,'o')
+# # plt.plot(qdotInst,qdotFmax,'o')
 plt.show()
+
 
