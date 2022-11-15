d1703a4013eadf02ccf3be0fcb65cdff38841032
Josh Whitehead <j.whitehead@co-dx.com>
2022-11-15 12:48:54 -0700

    's'

--- analysis/qdot/statsFmaxNoise.py
+++ analysis/qdot/statsFmaxNoise.py
@@ -4,7 +4,7 @@ import pandas as pd
 import importData
 
 data = pd.read_csv('pmmv_255.csv')
-
+data.dropna()
 inst = (data['inst']).tolist()
 fmax = (data['fmax']).tolist()
 noisePcr = (data['pcr noise']).tolist()
@@ -21,7 +21,7 @@ meltGood = []
 cqGood = []
 tmGood = []
 for i in range(len(inst)):
-    if str(noisePcr[i]) != 'nan' and str(noiseMelt[i]) != 'nan' and str(fmax[i] != 'nan') and str(cq[i]) != 'nan' and str(tm[i]) != 'nan' and str(noiseMelt[i]) != '  ' and str(cq[i]) != ' ' and str(tm[i]) != ' ':
+    if fmax[i] > 0 and str(noiseMelt[i]) != '  ' and str(cq[i]) != ' ' and str(tm[i]) != ' ':
         fmaxGood.append(fmax[i])
         instGood.append(inst[i])
         pcrGood.append(float(noisePcr[i]))
@@ -30,7 +30,6 @@ for i in range(len(inst)):
         cqGood.append(float(cq[i]))
 
         tmGood.append(float(tm[i]))
-
 
 
 
@@ -67,9 +66,63 @@ for i in range(len(importData.inst)):
         qdotInst.append(importData.inst[i])
         qdotFmax.append(importData.avg[i])
 
+
+
+
 fmaxMean.remove(fmaxMean[3])
-plt.plot(qdotFmax,fmaxMean,'o')
-# # plt.plot(qdotInst,qdotFmax,'o')
+pcrMean.remove(pcrMean[3])
+meltMean.remove(meltMean[3])
+cqMean.remove(cqMean[3])
+
+
+
+
+
+# plt.plot(qdotFmax,fmaxMean,'o')
+# plt.plot(qdotFmax,fmaxMean,'o')
+# plt.grid()
+# plt.show()
+
+# plt.plot(qdotFmax,pcrMean,'o')
+# plt.grid()
+# plt.show()
+
+# plt.plot(qdotFmax,meltMean,'o')
+# plt.grid()
+# plt.show()
+
+# plt.plot(qdotFmax,cqMean,'o')
+# plt.grid()
+# plt.show()
+
+a,b = np.polyfit(qdotFmax,fmaxMean,1)
+c,d = np.polyfit(qdotFmax,pcrMean,1)
+e,f = np.polyfit(qdotFmax,meltMean,1)
+g,h = np.polyfit(qdotFmax,cqMean,1)
+
+
+qdotFmax = np.array(qdotFmax)
+print(np.mean(qdotFmax))
+
+
+fig,ax = plt.subplots(2,2)
+ax[0,0].plot(qdotFmax,fmaxMean,'o')
+ax[0,1].plot(qdotFmax,pcrMean,'o')
+ax[1,0].plot(qdotFmax,meltMean,'o')
+ax[1,1].plot(qdotFmax,cqMean,'o')
+ax[0,0].plot(qdotFmax,a*qdotFmax+b)
+ax[0,1].plot(qdotFmax,c*qdotFmax+d)
+ax[1,0].plot(qdotFmax,e*qdotFmax+f)
+ax[1,1].plot(qdotFmax,g*qdotFmax+h)
+ax[0,0].title.set_text('fmax')
+ax[0,1].title.set_text('pcr noise')
+ax[1,0].title.set_text('melt noise')
+ax[1,1].title.set_text('cq')
+ax[0,0].grid()
+ax[0,1].grid()
+ax[1,0].grid()
+ax[1,1].grid()
 plt.show()
+
 
 
