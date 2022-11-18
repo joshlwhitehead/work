6a2ad5ac6618d48c88d608fee99bd4a4120fb355
Josh Whitehead <j.whitehead@co-dx.com>
2022-11-18 13:57:46 -0700

    'h'

--- analysis/qdot/statsFmaxNoise.py
+++ analysis/qdot/statsFmaxNoise.py
@@ -6,14 +6,26 @@ from statsmodels.formula.api import ols
 from statsmodels.stats.anova import anova_lm
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
-data = pd.read_csv('pmmvAll_16Nov2022.csv')
+data = pd.read_csv('test.csv')
 data.dropna()
 inst = (data['inst']).tolist()
 fmax = (data['fmax']).tolist()
-noisePcr = (data['pcr noise']).tolist()
-noiseMelt = (data['melt noise']).tolist()
-cq = (data['cq']).tolist()
-tm = (data['tm']).tolist()
+if 'pcr noise' in data.columns():
+    noisePcr = (data['pcr noise']).tolist()
+else:
+    noisePcr = []
+if 'melt noise' in data.columns():
+    noiseMelt = (data['melt noise']).tolist()
+else:
+    noiseMelt = []
+if 'cq' in data.columns():
+    cq = (data['cq']).tolist()
+else:
+    cq = []
+if 'tm' in data.columns():
+    tm = (data['tm']).tolist()
+else:
+    tm = []
 
 
 
@@ -24,17 +36,15 @@ meltGood = []
 cqGood = []
 tmGood = []
 for i in range(len(inst)):
-    if str(noiseMelt[i]) != '  ' and str(cq[i]) != ' ' and str(tm[i]) != ' ' and fmax[i] > 0:
+    if str(noiseMelt[i]) != '  ' and str(cq[i]) != ' ' and str(tm[i]) != ' ' and fmax[i] > 0 and str(cq[i]) != 'nan':
         fmaxGood.append(fmax[i])
         instGood.append(inst[i])
         pcrGood.append(float(noisePcr[i]))
-        # print(noiseMelt[i])
         meltGood.append(float(noiseMelt[i]))
         cqGood.append(float(cq[i]))
-
         tmGood.append(float(tm[i]))
 
-dfAnova = pd.DataFrame({'inst':instGood,'fmax':fmaxGood})
+dfAnova = pd.DataFrame({'inst':instGood,'fmax':fmaxGood,'cq':cqGood,'pcr noise':pcrGood,'melt noise':meltGood})
 dfAnova = dfAnova.dropna()
 dfAnova.boxplot('fmax',by='inst')
 plt.show()
@@ -48,17 +58,17 @@ print(aov_table)
 
 m_comp = pairwise_tukeyhsd(endog=dfAnova['fmax'], groups=dfAnova['inst'], 
                            alpha=0.05)
-print(m_comp.reject)
-# print(dfAnova['inst'].value_counts())
+print(m_comp)
+
 qdotInst = []
-qdotFmax = []
+qdotRat = []
 for i in range(len(importData.inst)):
     if importData.inst[i] in instSimp:
         qdotInst.append(importData.inst[i])
-        qdotFmax.append(importData.avg[i])
+        qdotRat.append(importData.avg[i])
 
 
-print(instSimp)
+
 count = 0
 while len(instSimp) != len(qdotInst):
     if qdotInst[count] != instSimp[count]:
@@ -110,61 +120,68 @@ for i in instSimp:
 # plt.grid()
 # plt.show()
 
-# plt.plot(qdotFmax,fmaxMean,'o')
-# plt.plot(qdotFmax,fmaxMean,'o')
+# plt.plot(qdotRat,fmaxMean,'o')
+# plt.plot(qdotRat,fmaxMean,'o')
 # plt.grid()
 # plt.show()
 
-# plt.plot(qdotFmax,pcrMean,'o')
+# plt.plot(qdotRat,pcrMean,'o')
 # plt.grid()
 # plt.show()
 
-# plt.plot(qdotFmax,meltMean,'o')
+# plt.plot(qdotRat,meltMean,'o')
 # plt.grid()
 # plt.show()
 
-# plt.plot(qdotFmax,cqMean,'o')
+# plt.plot(qdotRat,cqMean,'o')
 # plt.grid()
 # plt.show()
-# print(len(qdotFmax),len(fmaxMean))
+# print(len(qdotRat),len(fmaxMean))
+
+qdotAdj = []
+fmaxAdj = []
+use = [5,6,7,8,9,10,25,26]
+for i in range(len(qdotInst)):
+    if qdotInst[i] in use:
+        qdotAdj.append(qdotRat[i])
+        fmaxAdj.append(fmaxMean[i])
 
 
-print(instSimp,'\n',qdotInst)
-a,b = np.polyfit(qdotFmax,fmaxMean,1)
-# c,d = np.polyfit(qdotFmax,pcrMean,1)
-# e,f = np.polyfit(qdotFmax,meltMean,1)
-# g,h = np.polyfit(qdotFmax,cqMean,1)
-print(fmaxMean)
+a,b = np.polyfit(qdotAdj,fmaxAdj,1)
+c,d = np.polyfit(qdotRat,pcrMean,1)
+e,f = np.polyfit(qdotRat,meltMean,1)
+g,h = np.polyfit(qdotRat,cqMean,1)
 
-qdotFmax = np.array(qdotFmax)
 
-plt.plot(qdotFmax,fmaxMean,'o')
-plt.plot(qdotFmax,a*qdotFmax+b)
-plt.grid()
+qdotRat = np.array(qdotRat)
+
+# plt.plot(qdotRat,fmaxMean,'o')
+# plt.plot(qdotRat,a*qdotRat+b)
+# plt.grid()
+# plt.show()
+
+
+# print(cqGood)
+
+
+fig,ax = plt.subplots(2,2)
+ax[0,0].plot(qdotAdj,fmaxAdj,'o')
+ax[0,1].plot(qdotRat,pcrMean,'o')
+ax[1,0].plot(qdotRat,meltMean,'o')
+ax[1,1].plot(qdotRat,cqMean,'o')
+ax[0,0].plot(qdotRat,a*qdotRat+b)
+ax[0,1].plot(qdotRat,c*qdotRat+d)
+ax[1,0].plot(qdotRat,e*qdotRat+f)
+ax[1,1].plot(qdotRat,g*qdotRat+h)
+ax[0,0].title.set_text('fmax')
+ax[0,1].title.set_text('pcr noise')
+ax[1,0].title.set_text('melt noise')
+ax[1,1].title.set_text('cq')
+ax[0,0].grid()
+ax[0,1].grid()
+ax[1,0].grid()
+ax[1,1].grid()
 plt.show()
-
-
-
-
-
-# fig,ax = plt.subplots(2,2)
-# ax[0,0].plot(qdotFmax,fmaxMean,'o')
-# ax[0,1].plot(qdotFmax,pcrMean,'o')
-# ax[1,0].plot(qdotFmax,meltMean,'o')
-# ax[1,1].plot(qdotFmax,cqMean,'o')
-# ax[0,0].plot(qdotFmax,a*qdotFmax+b)
-# ax[0,1].plot(qdotFmax,c*qdotFmax+d)
-# ax[1,0].plot(qdotFmax,e*qdotFmax+f)
-# ax[1,1].plot(qdotFmax,g*qdotFmax+h)
-# ax[0,0].title.set_text('fmax')
-# ax[0,1].title.set_text('pcr noise')
-# ax[1,0].title.set_text('melt noise')
-# ax[1,1].title.set_text('cq')
-# ax[0,0].grid()
-# ax[0,1].grid()
-# ax[1,0].grid()
-# ax[1,1].grid()
-# plt.show()
 
 
 
