40bdd11ee3d3a1b35a281b23870efefa5c352c7e
Josh Whitehead <j.whitehead@co-dx.com>
2022-12-12 12:02:03 -0700

    new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv01_DV03_220907_Run1.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv01_DV03_220907_Run2.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv01_DV03_220907_Run3.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv01_W61_220919_Run1.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv01_W61_220919_Run2.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv01_W61_220919_Run3.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv02_DV03_220907_Run1.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv02_DV03_220907_Run2.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv02_DV03_220907_Run3.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv04_W61_220919_Run1.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv04_W61_220919_Run2.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv04_W61_220919_Run3.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv05_W65_220929_Run1.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv05_W65_220929_Run2.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv05_W65_220929_Run3.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv08_W61_220920_Run1.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv08_W61_220920_Run2.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv08_W61_220920_Run3.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv11_W61_220921_Run1.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv11_W61_220921_Run2.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv11_W61_220921_Run3.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv14_W61_220920_Run1.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv14_W61_220920_Run2.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv14_W61_220920_Run3.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv16_W61_220920_Run1.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv16_W61_220920_Run2.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv16_W61_220920_Run3.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv18_W65_220929_Run1.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv18_W65_220929_Run2.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv18_W65_220929_Run3.csv 	renamed:    analysis/heat/dataqstuff/dataCollect/pars/fileMade/Adv26_DV05_221020_Run2.csv -> analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv26_DV05_221020_Run2.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv3_DV03_220829_Run01.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv3_DV03_220829_Run02.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv3_DV03_220829_Run03.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv4_DV09_220829_Run01.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv4_DV09_220829_Run02.csv 	new file:   analysis/heat/dataqstuff/dataCollect/data/08Dec2022/Adv4_DV09_220829_Run03.csv 	deleted:    analysis/heat/dataqstuff/dataCollect/pars/fileToUse/Adv26_DV05_221020_Run2.txt 	modified:   analysis/heat/dataqstuff/dataCollect/thermalValPCR.py

--- analysis/heat/dataqstuff/dataCollect/thermalValPCR.py
+++ analysis/heat/dataqstuff/dataCollect/thermalValPCR.py
@@ -14,7 +14,7 @@ from statsmodels.graphics.factorplots import interaction_plot
 
 def DV():
     total = dat.DV
-    instListShort = [1,2,3,4,6,7,10,13,15,25,26,27]
+    instListShort = [1,2,3,4,26]
     instList = instListShort*3
     instList.sort()
     cupList = [3,3,3,3,3,3,3,3,3,9,9,9,8,8,8,8,8,8,10,10,10,5,5,5,5,5,5,8,8,8,5,5,5,8,8,8]
@@ -63,8 +63,72 @@ def DV():
             count += 1
         modelMax.append(maxes)
         sampleMax.append(maxes2)
-    print(sampleMax)
-DV()
+    
+
+    for i in range(len(modelMax)):
+        plt.plot(modelMax[i],'k')
+        plt.plot(sampleMax[i],'b')
+    plt.show()
+
+
+def wet():
+    total = dat.wet
+    instListShort = [1,2,3,4,26]
+    instList = instListShort*3
+    instList.sort()
+    cupList = [3,3,3,3,3,3,3,3,3,9,9,9,8,8,8,8,8,8,10,10,10,5,5,5,5,5,5,8,8,8,5,5,5,8,8,8]
+    cupListClump = [3,3,3,9,8,8,10,5,5,8,5,8]
+    
+    colors = ['blue','crimson','green','orange','purple','cyan','deeppink','gray','brown','olive','peru','maroon']
+
+    cycle = 6
+    modelMax = []
+    sampleMax = []
+    modelStuff = []
+    sampleStuff = []
+    
+    
+    for k in total:
+        maxes = []
+        maxes2 = []
+        indx = []
+        count = 0
+        for i in k[5]:
+            if i == 90:
+                indx.append(count)
+            count += 1
+
+
+
+        modelNew = []
+        modelLists = []
+        sampleNew = []
+        sampleLists = []
+        count = 0
+        for i in indx:
+            if indx[count] != indx[-1] and i+1 == indx[count+1]:
+                modelNew.append(k[4][i])
+                sampleNew.append(k[2][i])
+            else:
+                modelLists.append(modelNew)
+                sampleLists.append(sampleNew)
+                sampleNew = []
+                modelNew = []
+            count += 1
+        count = 2
+        for i in modelLists[2:-1]:
+            maxes.append(max(i))
+            maxes2.append(max(sampleLists[count]))
+            count += 1
+        modelMax.append(maxes)
+        sampleMax.append(maxes2)
+    
+
+    for i in range(len(modelMax)):
+        plt.plot(modelMax[i],'k')
+        plt.plot(sampleMax[i],'b')
+    plt.show()
+wet()
 
                 
 
