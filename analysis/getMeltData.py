ea3ac1a743173d696e12a13f7065486c618a8d6d
Josh Whitehead <j.whitehead@co-dx.com>
2023-02-02 17:23:12 -0700

    new file:   analysis/__pycache__/getMeltData.cpython-310.pyc 	new file:   analysis/analyzeMelt.py 	modified:   analysis/explained.ipynb 	new file:   analysis/getMeltData.py 	new file:   analysis/idea.png 	modified:   analysis/specSigMelt.py

--- analysis/getMeltData.py
+++ analysis/getMeltData.py
@@ -7,18 +7,23 @@ import numpy as np
 from lib.consumable_config import getConfigMap
 from melt.melt import MeltAnalysis
 from scipy.interpolate import make_interp_spline
+import time
+import pandas as pd
+from scipy import stats
 
 
 consumableId = ['cb33b22a-492a-4efa-83bf-6b232bc406c1','9b3a4660-7514-4d36-86a5-6ab76d427bc1','1eb72362-7fc4-4028-b294-027eb64cd4a9','054efdec-1dae-4c57-8c6b-48748f69b514','eaf107b4-36aa-463d-8270-6191e2128ed3']
-for i in consumableId:
-    rep = 1
+def getMelt(consumableId,memoForRep):
+
+    
+    
 
 
     api = auth.getApiClient()
 
     where = {
     'id': {
-        '_eq': i
+        '_eq': consumableId
     }
     }
     channels = np.array([515,555,590,630,680])
@@ -26,12 +31,23 @@ for i in consumableId:
 
 
     response = api.execute(query=qry.consumableWhere,variables={'where':where})
+
+
     # print(response)
+    repMem = {}
+    for i in range(len(response['data']['consumable'][0]['experiments'])):
+        repMem[response['data']['consumable'][0]['experiments'][i]['memo']] = i
+    
+    rep = repMem[memoForRep]
+
+    # melts = []
 
     rawData = response['data']['consumable'][0]['experiments'][rep]['experimentResult']['pcrData']['sensor']
+    
     data = np.array(rawData).T.tolist()
     meltData = response['data']['consumable'][0]['experiments'][rep]['experimentResult']['meltData']['sensor']
     data_melt = np.array(meltData).T.tolist()
+    # melts.append(data_melt)
     # print(meltData.keys())
     if len(data_melt) > 0:
         tData = response ['data']['consumable'][0]['experiments'][rep]['experimentResult']['meltData']['temperature']
@@ -43,47 +59,36 @@ for i in consumableId:
     if len(data_melt) > 0:
         meltAnalysis.callMelt(tData, mData)
 
-    print(i[:6])
-    for u in data_melt[1:3]:
-        print('mean: ',np.mean(u))
-        print('base: ',u[-1])
-    # print('\n')
+    return data_melt
 
 
 
+LED = []
+detect = []
+both = []
+y = {}
+nominal = []
+flip = []
+memos = ['filter flip 0','filter flip 1 LED','filter flip 2 detector','filter flip 3 both']
+wave = 3
+count = 0
+
+for j in memos:
+    for l in consumableId:
+        # print(l)
+        x445 = max(getMelt(l,j)[wave])/min(getMelt(l,j)[wave])
+        # x445 = (max(getMelt(l,j)[wave])-min(getMelt(l,j)[wave]))/np.mean(getMelt(l,j)[wave])
+        # x445 = max(getMelt(l,j)[wave])
+        nominal.append(x445)
+        flip.append(count)
+    y['flip'] = flip
+    y['baseline'] = nominal
+    count += 1
+print(y)
+dF = pd.DataFrame(y)
+
+# dF.hist('baseline')
+# plt.show()
 
 
 
-
-
-
-
-
-def derivSpec():
-    x = channels
-    y = []
-    for i in [3,4,5,6,7]:
-        y.append(max(meltAnalysis.results['smoothDerivatives'][i]))
-    y = np.array(y)
-
-    X_Y_Spline = make_interp_spline(channels,y)
-
-   
-    X_ = np.linspace(x.min(), x.max(), 500)
-    Y_ = X_Y_Spline(X_)
-
-    # plt.plot(meltAnalysis.results['smoothedT'],meltAnalysis.results['smoothDerivatives'][4])
-    plt.figure()
-    plt.plot(X_,Y_)
-    plt.plot(channels,y,'o')
-    plt.title(''.join(['Spectral Signature ',consumableId[:6]]))
-    # plt.ylabel(''.join(["Melt Peak @ ",str(round(good_tm,1))]))
-    plt.xlabel('Channel (nm)')
-    # plt.ylim(-.15,2.2)
-    plt.xticks((515,555,590,630,680),('515','555','590','630','680'))
-    plt.grid()
-    
-    
-    # plt.savefig(''.join(['specSigMadison/21Jul2022/',consumableId[:6]]))
-    plt.show()
-# derivSpec()
