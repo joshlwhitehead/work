4438b13146e5e9a186a3fb929f1da1e5542f9469
Josh Whitehead <j.whitehead@co-dx.com>
2023-02-16 17:20:11 -0700

    modified:   analysis/heat/dataqstuff/dataCollect/TIAnalysis/Instructions.docx 	modified:   analysis/heat/dataqstuff/dataCollect/TIAnalysis/TC_TI.py 	modified:   analysis/heat/dataqstuff/dataCollect/TIAnalysis/TC_rampRate.py 	deleted:    analysis/heat/dataqstuff/dataCollect/TIAnalysis/~$structions.docx 	deleted:    analysis/heat/dataqstuff/dataCollect/TIAnalysis/~WRL0587.tmp 	new file:   analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/PCR_TI.py 	new file:   analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/PCR_rampRate.py 	new file:   analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/TC_TI.py 	new file:   analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/TC_rampRate.py 	new file:   analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/dataPCR/proposedModel_Adv15_wxx_120123_Run1.txt 	new file:   analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/dataPCR/proposedModel_Adv25_wxx_120123_Run1.txt 	new file:   analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/dataTC/Adv02_P11_221215_Run2.txt 	new file:   analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/dataTC/Adv06_P11_2383ea_221220_Run1.txt 	new file:   analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/parsTxt.py

--- analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/TC_TI.py
+++ analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/TC_TI.py
@@ -47,9 +47,9 @@ def kill():
         plt.hlines(count,bound[0][0],bound[0][1],lw=5)
         
         if bound[0][0] < killTemp-deviationCrit or bound[0][1] > killTemp+deviationCrit:          #pass if TI within acceptance criteria
-            print(instListVar[count],'TI:',bound,'FAIL')
+            print(instListVar[count],bound,'FAIL')
         else:
-            print(instListVar[count],'TI:',bound,'PASS')
+            print(instListVar[count],bound,'PASS')
         count += 1
     plt.yticks(np.arange(0,len(temp)),instListVar)
     plt.vlines(killTemp+deviationCrit,0,count,'k',lw=5)
@@ -86,66 +86,6 @@ def kill():
     plt.show()
 
 
-def act():
-    instListVar = []
-    for inst in instListShort:
-        for rep in range(replicate):
-            instListVar.append(''.join([str(inst),'.',str(rep)]))
-    
-
-    temp = []
-    means = []
-    count = 0
-    for file in os.listdir(folder):
-        actTemps = parsTCTxt(''.join([folder,file]))[1][0]
-        temp.append(actTemps)
-        mean = np.mean(actTemps)
-        means.append(mean)
-        actTemp = parsTCTxt(''.join([folder,file]))[2][1]
+kill()
 
 
-        bound = ti.twoside.normal(actTemps,p,1-alpha)                                               #tolerance interval for each run                                                                        #list of TIs
-
-        plt.hlines(count,bound[0][0],bound[0][1],lw=5)
-        
-        if bound[0][0] < actTemp-deviationCrit or bound[0][1] > actTemp+deviationCrit:          #pass if TI within acceptance criteria
-            print(instListVar[count],'TI:',bound,'FAIL')
-        else:
-            print(instListVar[count],'TI:',bound,'PASS')
-        count += 1
-    plt.yticks(np.arange(0,len(temp)),instListVar)
-    plt.vlines(actTemp+deviationCrit,0,count,'k',lw=5)
-    plt.vlines(actTemp-deviationCrit,0,count,'k',lw=5)
-    plt.title('95% Tolerance Intervals (p=0.90)')
-    plt.ylabel('Instrument')
-    plt.xlabel('Temperature (c)')
-    plt.grid()
-    plt.show()
-
-
-
-
-    count=0
-    for data in temp:
-        mean_er = np.mean(data)                                                     #mean denature temp of run
-        std_dev_er = np.std(data)                                                   #stdev of each run
-        n = len(data)                                                               
-        se = std_dev_er / np.sqrt(n)                                                #standard error for t-test stat
-        dof = n - 1                                                                 #degree of fredom
-        t_star = stats.t.ppf(1.0 - 0.5 * alpha, dof)                                #t* get from t-dist ppf
-        moe = t_star * se                                                           #margin of error
-        ciMult = np.array([mean_er - moe, mean_er + moe])                           #1-alpha confidence interval
-        print(instListVar[count],'CI:',round(mean_er,3),'+/-',round(moe,4))
-        plt.hlines(count,ciMult[0],ciMult[1],lw=5)                                  #plot confidence interval
-        plt.plot(mean_er,count,'o',color='r',ms=7)
-        count+=1
-
-    plt.yticks(np.arange(0,len(temp)),instListVar)
-    plt.title(''.join([str((1-alpha)*100),'% Confidence Interval']))
-    plt.grid()
-    plt.xlabel('Mean Temp (c)')
-    plt.ylabel('Instrument')
-    plt.show()
-
-
-act()
---
+++ analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/parsTxt.py
@@ -0,0 +1,208 @@
+"""This script reads a .txt file generated from the DAVE tool during thermal runs. The script then parses out the thermocouple data.
+The first function returns lists of temps while heating and lists of temps while cooling during the PCR stage. The lists are embedded within
+another list, clumped by cycle number (ie. each cycle has a seperate list of heating/cooling temps).
+The second function returns a single list of heat kill temperatures and a single list of activation temperatures during the Treatment stage.
+Both functions also return the set temps used."""
+
+def parsPCRTxt(file):                                                                                                       #read PCR data
+
+    with open(file,'r') as readFile:                                                                                        #convert lines in file to list
+        file = readFile.readlines()
+
+
+    heat = [[]]                      #temp (c) while PCR is heating
+    timeH = [[]]                     #time (sec) while PCR is heating
+    cool = [[]]                      #temp (c) while PCR is cooling
+    timeC = [[]]                     #time (sec) while PCR is cooling
+    heatCollect = False                                                                                                 #key to start collecting temps while heating
+    coolCollect = False                                                                                                 #key to start collecting temps while cooling
+    start = False                                                                                                       #key to know when to start collecting temps
+    countLines = 0
+    countH = 0
+    countC = 0
+
+    for u in file:
+        if 'Start PCR' in u:
+            start = True                                                                                                #start looking for temps
+        if 'goto' in u and 'Controlled' not in u and float(u.split()[-1]) > 85:                                         #only look for temps under these conditions for heating
+            denatTemp = float(u.split()[-1])
+            heatCollect = True
+            coolCollect = False
+            # heat.append([])
+        elif 'goto' in u and 'Controlled' not in u and float(u.split()[-1]) < 65 and float(u.split()[-1]) >= 45:        #look for temps under these conditions for cooling
+            annealTemp = float(u.split()[-1])
+            heatCollect = False
+            coolCollect = True
+        elif 'MELT' in u:                                                                                               #start looking for temps
+            start = False
+
+        if start and 'DATAQ:' in u:                                                                                     #only collect temps in these lines
+
+            try:
+                if heatCollect and len(heat[countH]) != 0 and heat[countH][-1]-float(u.split()[4].strip(',')) > 10:     #if criteria met make a new index in heating lists
+                    heat.append([])
+                    timeH.append([])
+                    countH+=1
+                elif coolCollect and len(cool[countC]) != 0 and float(u.split()[4].strip(','))-cool[countC][-1] > 10:   #if criteria met make a new index in cooling lists
+                    cool.append([])
+                    timeC.append([])
+                    countC+=1
+            except:
+                pass
+            
+            
+            if heatCollect:                                                                                             #start collecting heating temps
+                heat[countH].append(float(u.split()[4].strip(',')))
+                try:
+                    timeH[countH].append(float(file[countLines-1].split()[0].strip('()'))/1000)
+                except:
+                    timeH[countH].append(float(file[countLines+1].split()[0].strip('()'))/1000)
+
+            elif coolCollect:                                                                                          #start collecting cooling temps
+                cool[countC].append(float(u.split()[4].strip(',')))
+                try:
+                    timeC[countC].append(float(file[countLines-1].split()[0].strip('()'))/1000)
+                except:
+                    timeC[countC].append(float(file[countLines+1].split()[0].strip('()'))/1000)
+        
+        
+        countLines += 1
+
+
+    count = 0
+    for j in range(len(heat)):                                         #remove heating arrays with too few values
+        if len(heat[count]) <= 2:
+            heat.remove(heat[count])
+            timeH.remove(timeH[count])
+            count += 1
+    count = 0      
+    for j in range(len(cool)):                                          #remove cooling arrays with too few values
+        if len(cool[count]) <= 2:
+            cool.remove(cool[count])
+            timeC.remove(timeC[count])
+            count += 1
+
+            
+    return (heat,timeH),(cool,timeC),(denatTemp,annealTemp)             #return heating temps and times, cooling temps and times, and the set temps for denature and anneal
+
+
+
+
+
+def parsTCTxt(file):                                                                    #function that reads treatment chamber data
+
+    
+
+    with open(file,'r') as readFile:                                                    #read file into a list
+        file = readFile.readlines()
+
+
+    kill = []                              #temp (c) during heat kill step
+    timeK = []                              #time (sec) for heat kill step
+    act = []                                #temp (c) for activation step
+    timeA = []                              #time (sec) for activation step
+    killCollect = False                                                                 #keys to determine when to start/stop looking for the desired data
+    actCollect = False
+    start = True
+    start2 = False
+    countLines = 0
+
+
+
+
+
+    for u in file:
+        if 'Switching to steady' in u:                                                     #this criteria indicates start of data
+            start2 = True
+        if 'goto' in u and 'Controlled' not in u and float(u.split()[-1]) > 85:             #this criteria indicates start of kill temp collection
+            killTemp = float(u.split()[-1])
+            killCollect = True
+            actCollect = False
+            start2 = False
+            # heat.append([])
+        elif 'goto' in u and 'Controlled' not in u and float(u.split()[-1]) < 75 and float(u.split()[-1]) >= 55:        #this criteria indicates start of activation temp collect
+            actTemp = float(u.split()[-1])
+            killCollect = False
+            actCollect = True
+        elif 'Using Cooling Equations' in u:
+            start = False
+
+        if start and start2 and 'DATAQ:' in u:                                                  #start collecting data
+
+            
+            if killCollect:
+                kill.append(float(u.split()[4].strip(',')))                                         #collect kill data
+                try:
+                    timeK.append(float(file[countLines-1].split()[0].strip('()'))/1000)
+                except:
+                    print(file[countLines+1].split())
+                    timeK.append(float(file[countLines+1].split()[0].strip('()'))/1000)
+
+            elif actCollect:                                                                            #collect activation data
+                act.append(float(u.split()[4].strip(',')))
+                try:
+                    timeA.append(float(file[countLines-1].split()[0].strip('()'))/1000)
+                except:
+                    timeA.append(float(file[countLines+1].split()[0].strip('()'))/1000)
+        
+        
+        countLines += 1
+        
+            
+    return (kill,timeK),(act,timeA),(killTemp,actTemp)                          #return kill temps and times, activation temps and times, and set temps for heat kill and activation step
+
+def TCramp(file):
+    with open(file,'r') as readFile:                                                    #read file into a list
+        file = readFile.readlines()
+    heatRR1 = []
+    timeH1 = []
+    heatRR2 = []
+    timeH2 = []
+    coolRR = []
+    timeC = []
+
+    masterStop = False
+    startHeat1 = False
+    startHeat2 = False
+    startCool = False
+    countLines = 0
+
+    for u in file:
+        if 'goto' in u and float(u.split()[-1]) > 85:
+            startHeat2 = True
+        elif 'goto' in u and float(u.split()[-1]) < 75 and float(u.split()[-1]) > 55:
+            startHeat1 = True
+        elif 'goto' in u and float(u.split()[-1]) <55:
+            startCool = True
+        if 'Set Temp Reached' in u:
+            startCool = False
+            startHeat1 = False
+            startHeat2 = False
+        if 'Start Heating PCR' in u:
+            masterStop = True
+        
+        if 'DATAQ:' in u and not masterStop:
+            if startHeat1:
+                heatRR1.append(float(u.split()[4].strip(',')))
+                try:
+                    timeH1.append(float(file[countLines-1].split()[0].strip('()'))/1000)
+                except:
+                    timeH1.append(float(file[countLines+1].split()[0].strip('()'))/1000)
+            elif startHeat2:
+                heatRR2.append(float(u.split()[4].strip(',')))
+                try:
+                    timeH2.append(float(file[countLines-1].split()[0].strip('()'))/1000)
+                except:
+                    timeH2.append(float(file[countLines+1].split()[0].strip('()'))/1000)
+            elif startCool:
+                coolRR.append(float(u.split()[4].strip(',')))
+                try:
+                    timeC.append(float(file[countLines-1].split()[0].strip('()'))/1000)
+                except:
+                    timeC.append(float(file[countLines+1].split()[0].strip('()'))/1000)
+
+
+        countLines += 1
+
+    return (heatRR1,timeH1),(heatRR2,timeH2),(coolRR,timeC)
+# print(TCramp('dataTC/Adv02_P11_221215_Run2.txt')[0][0])
