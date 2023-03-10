166c15fde3509c9cad00baa80cb9374597d4af9d
Josh Whitehead <j.whitehead@co-dx.com>
2023-03-10 10:28:43 -0700

    's'

--- analysis/heat/dataqstuff/modelV4/transient.py
+++ analysis/heat/dataqstuff/modelV4/transient.py
@@ -1,26 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+from scipy.optimize import fsolve
+from scipy.interpolate import interp1d
+
+data = pd.read_csv('test.csv')
+therm = data['therm']
+time = data['time']
+samp = data['samp']
+# plt.figure()
+# plt.plot(time,samp)
+# plt.grid()
+# plt.show()
+
 
 def interpBasic(y1,y2,x,x1,x2):
     y = y1+(x-x1)*(y2-y1)/(x2-x1)
     return y
 
-Tinf = 90
-Ti = 20
+
+Tinf = 96
+Ti = 55
 k = .648
-h = 10
-L = .011
+h = 500
+L = .0011
 ro = 982.36
 cp = 4190
+alpha = k/ro/cp
 Bi = h*L/k
-x = np.linspace(0,L)
-t = np.linspace(0,100,10)
+x = np.linspace(0,L,5)
+t = np.linspace(0,10,10)
+def eigen(x):
+    
+    return x*np.tan(x)-Bi
+si = []
+for i in np.arange(1,100*np.pi,np.pi):
+    root = fsolve(eigen,i)
+    si.append(root[0])
+# print(si)
 
-si1 = interpBasic(.1987,.2425,Bi,.06,.04)
-si2 = interpBasic(3.1543,3.1606,Bi,.06,.04)
-si3 = interpBasic(6.2895,6.2927,Bi,.06,.04)
-si4 = interpBasic(9.429,9.4311,Bi,.06,.04)
-si = np.array([si1,si2,si3,si4])
+
 
 def theta(si,F0,xStar):
     ans = 0
@@ -30,10 +49,9 @@ def theta(si,F0,xStar):
 
     return ans*(Ti-Tinf)+Tinf
 
-# F = k*5/ro/cp/L**2
-# theta(si,F,x/L)
 
-for i in t:
-    plt.plot(x,theta(si,k*i/ro/cp/L**2,x/L))
-
+# plt.figure()
+for i in x:
+    plt.plot(t,theta(si,alpha*t/L**2,i/L))
+plt.grid()
 plt.show()
