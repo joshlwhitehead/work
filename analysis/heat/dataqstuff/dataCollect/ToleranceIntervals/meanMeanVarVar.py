80cf835344fca82f4b41d273c5d577c93606d329
JoshW <whitehead.josh98@gmail.com>
2023-04-26 17:04:22 -0600

    modified:   analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/meanMeanVarVar.py

--- analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/meanMeanVarVar.py
+++ analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/meanMeanVarVar.py
@@ -118,62 +118,6 @@ print(aov_table)
 m_compMult = pairwise_tukeyhsd(endog=dF['var'],groups=dF['type'],alpha=alpha)
 print(m_compMult)
 
-# plt.show()
-
-
-
-# x = np.linspace(min(var),max(var))
-# plt.hist(var,density=True)
-# plt.plot(x,t.pdf(x,loc=np.mean(var),scale=np.std(var),df=len(var)-1))
-# plt.show()
-
-
-
-
-
-# print(fullCI(folder2,instList2)[5])
-
-# for i in range(len(fullCI(folder2,instList2)[3])):
-#     plt.hlines(i,fullCI(folder2,instList2)[4][0][i],fullCI(folder2,instList2)[4][1][i],lw=5)
-#     plt.plot(fullCI(folder2,instList2)[3][i],i,'o',color='r')
-# plt.hlines(-1,fullCI(folder2,instList2)[1],fullCI(folder2,instList2)[2])
-
-# plt.yticks(np.arange(0,len(fullCI(folder2,instList2)[3])),instList2)
-# plt.grid()
-
-# plt.show()
-
-
-# plt.hlines(0,fullCI(folder,instlist)[1],fullCI(folder,instlist)[2],lw=5)
-
-# plt.hlines(1,fullCI(folder2,instList2)[1],fullCI(folder2,instList2)[2],lw=5)
-# plt.plot(fullCI(folder,instlist)[0],0,'o',color='r')
-# plt.plot(fullCI(folder2,instList2)[0],1,'o',color='r')
-# # plt.yticks([0,1],['No-Recess','Recess'])
-# plt.title(''.join([str((1-alpha)*100),'% Confidence Interval of the Mean']))
-# plt.xlabel('Temperature (c)')
-# plt.ylabel('')
-# plt.grid()
-# plt.show()
-# n1 = len(instlist)
-# n2 = len(instList2)
-
-# t1 = t.ppf(1.0 - 0.5 * alpha,n1-1)
-# t2 = t.ppf(1-.5*alpha,n2-1)
-
-# s1 = (fullCI(folder,instlist)[0] - fullCI(folder,instlist)[1])*np.sqrt(len(instlist))/t1
-# s2 = (fullCI(folder2,instList2)[0] - fullCI(folder2,instList2)[1])*np.sqrt(len(instList2))/t2
-
-# SE = np.sqrt(s1**2/n1+s2**2/n2)
-
-# dof = (s1**2/n1+s2**2/n2)**2/((s1**2/n1)**2/(n1-1) + (s2**2/n2)**2/(n2-1))
-
-# tStar = t.ppf(1-.5*alpha,dof)
-# moe = SE*tStar
-# meanDiff = fullCI(folder,instlist)[0] - fullCI(folder2,instList2)[0]
-
-# print(meanDiff-moe,meanDiff+moe)
-
 
 
 
