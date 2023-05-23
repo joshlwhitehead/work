cb1de2858ee6c472ac4bbef0324d1971b3b4b946
Josh Whitehead <j.whitehead@co-dx.com>
2023-05-23 17:49:00 -0600

    renamed:    analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/dataB/Adv32_tapeCup_env40c_1.txt -> analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/data/Adv32_tapeCup_env40c_1.txt 	renamed:    analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/dataB/Adv32_tapeCup_env40c_2.txt -> analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/data/Adv32_tapeCup_env40c_2.txt 	deleted:    analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/data/PThermo_AdvBuild13_w86_230306_run1.txt 	renamed:    analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/dataB/adv32_tapeCup_env40c_3.txt -> analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/data/adv32_tapeCup_env40c_3.txt 	renamed:    analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/dataC/adv32_tapeCup_envROOM_1.txt -> analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/data/adv32_tapeCup_envROOM_1.txt 	modified:   analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/heatSinkComp.py

--- analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/heatSinkComp.py
+++ analysis/heat/dataqstuff/dataCollect/ToleranceIntervals/heatSinkComp.py
@@ -2,3 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from parsTxt import parsPCRTxt
+import os
+def test (x,a,b,c):
+    return a*np.log(x+b)+c
+for i in os.listdir('data'):
+    x = np.array(parsPCRTxt(''.join(['data/',i]))[5][1])-parsPCRTxt(''.join(['data/',i]))[5][1][0]
+    y = parsPCRTxt(''.join(['data/',i]))[5][0]
+    popt,popc = curve_fit(test,x,y)
+
+    plt.plot(x,y)
+plt.show()
