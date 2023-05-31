fd16afadc3ef7c0f91e739445f7c0b5e184374e0
Josh Whitehead <j.whitehead@co-dx.com>
2023-05-31 17:45:20 -0600

    's'

--- analysis/heat/dataqstuff/modelV4/looseTC.py
+++ analysis/heat/dataqstuff/modelV4/looseTC.py
@@ -1,18 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from scipy.optimize import curve_fit
+
 
 
 def parse(file):
     with open(file,'r') as readFile:                                                                                        #convert lines in file to list
         file = readFile.readlines()
 
+    stop = False
     thermData = []
     thermTime = []
     tCoupData = []
     tCoupTime = []
     countLines = 0
     for i in file:
-        if 'DATAQ:' in i:
+        if 'DATAQ:' in i and not stop:
             tCoupData.append(float(i.split()[4].strip(',')))
             try:
                     tCoupTime.append(float(file[countLines-1].split()[0].strip('()'))/1000)
@@ -29,42 +33,119 @@ def parse(file):
                         except:
                             print(file[countLines])
                             tCoupTime.append(float(file[countLines-3].split()[0].strip('()'))/1000)
-        elif 'modeled' in i:
+        elif 'modeled' in i and not stop:
             thermData.append(float(i.split()[4].strip(',')))
             thermTime.append(float(i.split()[0].strip('()'))/1000)
+            if float(i.split()[4].strip(',')) > 114:
+                stop = True
         
         countLines += 1
     thermTime = np.array(thermTime)-thermTime[0]
     tCoupTime = np.array(tCoupTime)-tCoupTime[0]
     return (thermTime,thermData),(tCoupTime,tCoupData)
+def r2(y,fit):
+    y = np.array(y)
+    fit = np.array(fit)
+    st = sum((y-np.average(y))**2)
+    sr = sum((y-fit)**2)
+    r2 = 1-sr/st
+    return r2
 
-
-
-
+def test(x,a,b):
+    return a*np.log(x+8.11522678)+b
 
 
 
 file = 'data/JW_mod4_tcInMiddle_1.txt'
-file2 = 'data/JW_mod4_tcInMiddleWithCupFail_2.txt'
-file3 = 'data/JW_mod4_tcInSide_3.txt'
+
 
 x1,y1 = parse(file)[0]
 x2,y2 = parse(file)[1]
 
-x3,y3 = parse(file2)[0]
-x4,y4 = parse(file2)[1]
 
-x5,y5 = parse(file3)[0]
-x6,y6 = parse(file3)[1]
 
-plt.plot(x1,y1,color='k')
-plt.plot(x2,y2,label='open')
-plt.plot(x3,y3,color='k')
-plt.plot(x4,y4,label='cup')
-plt.plot(x5,y5,color='k')
-plt.plot(x6,y6,label='side')
-plt.legend()
-plt.grid()
+
+
+pop1,pcov1 = curve_fit(test,x1,y1)
+pop2,pcov2 = curve_fit(test,x2,y2)
+
+
+
+
+
+
+
+
+newX1 = interp1d(x1,y1)
+newX2 = interp1d(x2,y2)
+x11 = np.linspace(x1[0],x1[-1])
+y11 = newX1(x11)
+x22 = np.linspace(x2[0],x2[-1])
+y22 = newX2(x22)
+
+
+
+
+def d3fun(xt,a,b,c,d,e,f,g,h):
+    x,t = xt
+    return (a*np.log(t**b+c)+d)*(e*x**f+g)+h
+dx = [0,.0021]
+x = []
+t = []
+T = []
+
+for i in dx:
+    for u in y22:
+        x.append(i)
+
+y = [x11,x22]
+z = np.array([y11,y22])
+for i in y:
+    for u in i:
+        t.append(u)
+for i in z:
+    for u in i:
+        T.append(u)
+print(len(x),len(t),len(T))
+
+
+# pop3,pcov3 = curve_fit(d3fun,[x,t],T)
+
+
+from sklearn import linear_model
+import pandas as pd
+
+dftime = x22.tolist()*2
+dfdist = []
+for i in dx:
+    for u in x22:
+        dfdist.append(i)
+dftemp = []
+for i in z:
+    for u in i:
+        dftemp.append(u)
+
+dFx = {'time':dftime,'dist':dfdist,'temp':dftemp}
+dF = pd.DataFrame(dFx)
+# print(len(dftemp),len(dftime),len(dfdist))
+
+X = dF[['time', 'dist']]
+y = dF['temp']
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+timetime = np.linspace(min(x22),max(x22))
+distdist = np.linspace(0,0.0021)
+
+pre = regr.predict([[5,0.0021]])
+print(pre)
+
+time,dist = np.meshgrid(x11,dx)
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot_wireframe(time,dist,z,label='data')
+# ax.plot_wireframe(timetime,distdist,regr.predict([[timetime,distdist]]))
+# ax.plot_wireframe(time,dist,d3fun([time,dist],*pop3),color='orange',label='fit')
+ax.legend()
 plt.show()
-
-
--- analysis/math/chi2.py
+++ analysis/math/chi2.py
@@ -1,49 +1,34 @@
 from scipy.stats import chi2,norm
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 
-a = .7992
-b = -4.5558
+
+a = .8427
+b = -3.9455
 c = 1.2018
 d = 6.772
 z = 1.645
-s = .1
-yL = 93.5
-yU = 96.5
+s = 1.982
+yl = 19
+yb = 23
 
 
 
-def TIlow(ybar,s,z,x2,n):
-    x2 = a*(n-1)+b
-    k = z*np.sqrt((n-1)*(1+1/n)/x2)
-    return ybar-k*s
-def TIHigh(ybar,s,z,x2,n):
-    x2 = c*(n-1)+d
-    k = z*np.sqrt((n-1)*(1+1/n)/x2)
-    return ybar+k*s
 
-def TIlowForN(ybar,yL,s,z):
-    alpha = ((ybar-yL)/z/s)**2
-    return n*(1-alpha*a)-1/n-alpha*(b-a)
 
-def TIhighForN(ybar,yU,s,z):
-    alpha = ((yU-ybar)/z/s)**2
-    return n*(1-alpha*c)-1/n-alpha*(d-c)
+def n(a,b,x):
+    return (x**2-1)/(a*x**2-a*x+b*x) - ((yb-yl)/s/z)**2
 
-n = np.linspace(3,20)
 
-# plt.plot(n,TIhighForN(3.55e-149,.5,1e-148,z))
-# plt.plot(n,TIhighForN(3.55e-149,.03,1e-148,z))
-# plt.plot(n,TIlowForN(3.55e-149,.001,1e-148,z))
-s = .33
-plt.plot(n,TIlow(88.8,s,z,3,n))
-plt.plot(n,TIHigh(88.8,s,z,3,n))
-# plt.plot(n,TIlow(56.5,s,z,3,n))
-# plt.plot(n,TIlow(53.5,s,z,3,n))
-# plt.hlines(0,1,20)
+nnn = np.linspace(0,10,99)
+print(n(a,b,nnn),nnn)
+plt.plot(nnn,n(a,b,nnn))
 plt.grid()
-plt.legend()
 plt.show()
+plt.savefig('test.png')
 
-# print(TIHigh(3.55e-149,1e-148,z,2.733,9))
+    
+
+
+
+
