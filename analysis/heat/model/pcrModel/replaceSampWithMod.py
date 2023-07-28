dde6239edbc6d88086f486a7751a4c4be2fadd54
Josh Whitehead <j.whitehead@co-dx.com>
2023-07-28 10:56:37 -0600

    renamed:    analysis/heat/model/pcrModel/newFile.txt -> analysis/heat/model/pcrModel/doctoredPCR.txt 	modified:   analysis/heat/model/pcrModel/replaceSampWithMod.py 	modified:   analysis/heat/model/pcrModel/test.py

--- analysis/heat/model/pcrModel/replaceSampWithMod.py
+++ analysis/heat/model/pcrModel/replaceSampWithMod.py
@@ -13,13 +13,18 @@ with open('newFile.txt','w') as f:
     for indx,val in enumerate(file):
         if 'modeled' in val:
             modTot.append(val.split()[7].strip(','))
-        if ('CHUBE:' or 'DATAQ:') in val:
+        # print(val.split()[0])
+        if 'CHUBE:' in val or 'DATAQ:' in val:
+            
             current = val.split()
+            
             if len(modTot) > 0:
                 new = modTot[-1]
             else:
                 new = current[4]
             current[4] = new
+            
+            sampTot.append(new)
             current = ''.join([current[0],' ',current[1],' ',current[2],' ',
                                current[3],' ',current[4],'  ',current[5],'\n'])
             f.writelines(current)
@@ -28,4 +33,4 @@ with open('newFile.txt','w') as f:
             
         else:
             f.writelines(val)
-        
+print(sampTot)
--- analysis/heat/model/pcrModel/test.py
+++ analysis/heat/model/pcrModel/test.py
@@ -1,4 +1,3 @@
-josh = [1,2,3,2]
-# josh.pop(0)
-josh[0] = 0
-print(josh)
+josh = [2,3,2]
+if (1 or 0) in josh:
+    print('ok')
