0c29a8da2da5e5627d31f4a4168718bdb1db6bbf
Josh Whitehead <j.whitehead@co-dx.com>
2024-06-17 07:32:15 -0600

    tolerance

--- analysis/fluorimeter/fluoroStack/fluoro.py
+++ analysis/fluorimeter/fluoroStack/fluoro.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy import stats
+from scipy import stats,integrate
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -31,9 +31,8 @@ colors = {
     630:'orange',
     680:'r'
     }
-def plotSensor(wave):
-    
-    FWHM = {
+
+FWHM = {
         415:26,
         445:30,
         480:36,
@@ -44,9 +43,14 @@ def plotSensor(wave):
         680:52
         }
 
-    STD = {}
-    for i in FWHM:
-        STD[i] = fwhmToStd(FWHM[i])
+STD = {}
+for i in FWHM:
+    STD[i] = fwhmToStd(FWHM[i])
+def plotSensor(wave):
+    
+    
+
+    
 
     if wave == 'all':
 
@@ -54,8 +58,15 @@ def plotSensor(wave):
             tail = 2*FWHM[i]
             x = np.linspace(i-tail,i+tail,999)
             z = stats.norm.pdf(x,loc=i,scale=STD[i])
+            zz = stats.norm.pdf(x,loc=i-10,scale=STD[i])
+            zzz = stats.norm.pdf(x,loc=i+10,scale=STD[i])
             z *= peaks[i]/max(z)/max(peaks.values())
-            plt.plot(x,z,color=colors[i],label=i)
+            zz *= peaks[i]/max(zz)/max(peaks.values())
+            zzz *= peaks[i]/max(zzz)/max(peaks.values())
+            plt.plot(x,z,color=colors[i],lw=3,label=' '.join([str(i)]))
+            plt.plot(x,zz,color=colors[i],ls=':',lw=3)#,label=' '.join([str(i),'-10 nm']))
+            plt.plot(x,zzz,color=colors[i],ls='--',lw=3)#,label=' '.join([str(i),'+10 nm']))
+            
     else:
         for i in wave:
             x = np.linspace(i-2*FWHM[i],i+2*FWHM[i],999)
@@ -65,12 +76,15 @@ def plotSensor(wave):
             z *= peaks[i]/max(z)/max(peaks.values())
             zz *= peaks[i]/max(zz)/max(peaks.values())
             zzz *= peaks[i]/max(zzz)/max(peaks.values())
-            plt.plot(x,z,color=colors[i],lw=3,label=' '.join(['nominal',str(i)]))
-            plt.plot(x,zz,linestyle=':',color=colors[i],lw=3,label=' '.join([str(i),'+10 nm']))
-            plt.plot(x,zzz,linestyle='--',color=colors[i],lw=3,label=' '.join([str(i),'-10 nm']))
-    plt.vlines(479,0,.2,'k',ls='--',lw=3)
-    plt.vlines(476,0,.2,'k',lw=3)
-    plt.vlines(473,0,.2,'k',ls=':',lw=3)
+            plt.plot(x,z,color=colors[i],lw=3,label=' '.join([str(i)]))
+            # plt.plot(x,zz,linestyle=':',color=colors[i],lw=3)#,label=' '.join([str(i),'-10 nm']))
+            # plt.plot(x,zzz,linestyle='--',color=colors[i],lw=3,label='445 +10nm')#,label=' '.join([str(i),'+10 nm']))
+    # plt.vlines(479,0,.1,'k',ls='--',lw=3,label='sensor filter +3nm')
+    plt.vlines(476,0,.1,'k',lw=3,label='sensor filter')#,label='sensor filter')
+    # plt.vlines(473,0,.1,'k',ls=':',lw=3,label='sensor filter -3nm')
+    plt.fill_between(x,z,where=(x>=476),color='grey')
+    plt.xlabel('Wavelength (nm)')
+    plt.ylabel('Relative Sensitivity')
     plt.legend()
     plt.grid()
     plt.show()
@@ -87,13 +101,37 @@ sytoxx = data['sytox wave']
 excite = data['excite']
 emit = data['emit']
 
-# plt.plot(x,LEDy,label='440nm LED')
-# plt.plot(x,LEDFilty,label='LED filter')
-# plt.plot(x,sensFilty,label='sensor filter')
-# # plt.plot(sytoxx,excite,label='sytox excitation')
-# plt.plot(sytoxx,emit,label='sytox emission')
+# plt.plot(x,LEDy,lw=3,label='440nm LED')
+# plt.plot(x,LEDFilty,lw=3,label='LED filter')
+# plt.plot(x,sensFilty,lw=3,label='sensor filter')
+# plt.plot(sytoxx,excite,lw=3,label='sytox excitation')
+# plt.plot(sytoxx,emit,lw=3,label='sytox emission')
+# plt.xlabel('Wavelength (nm)')
+# plt.ylabel('Transmission (%)')
+# plt.ylabel
 # plt.grid()
 # plt.legend()
 # plt.show()
 
-plotSensor([445,480])
+# plotSensor([445])
+def pdf(x,wave,sensorShift):
+    y = stats.norm.pdf(x,loc=wave+sensorShift,scale=STD[wave])
+
+
+def detected(wave,lowLim,sensorShift):
+    x = np.linspace(wave-2*FWHM[wave],wave+2*FWHM[wave],999)
+    y = stats.norm.pdf(x,loc=wave+sensorShift,scale=STD[wave])
+    xx = np.linspace(lowLim,x[-1],len(y))
+    area = integrate.trapz(y,xx)
+    return area
+nom = detected(445,476,0)
+worst = detected(445,473,10)
+best = detected(445,479,-10)
+
+print('nom',nom)
+print('worst',worst)
+print('best',best)
+
+print(best/worst)
+
+
